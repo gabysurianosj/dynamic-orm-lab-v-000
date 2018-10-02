ca4a92ed91bbbc556cfce971772499562dfd677d
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-02 07:23:34 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -71,6 +71,11 @@ class InteractiveRecord
     value_name = attribute.values[0]
     
     sql = <<-SQL 
-      
+        SELECT * FROM #{table_name}
+        WHERE #{column_name} = ?
+        SQL
+        
+    DB[:conn].execute(sql, value_name);
+  end 
   
 end

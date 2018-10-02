3882fa4612f937e1f9a062c923f29b2015712622
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-02 07:18:29 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,15 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry' 
 
 class InteractiveRecord
+  
+  def self.table_name
+    self.to_s.downcase.pluralize 
+  end 
+  
+  def self.column_names 
+    DB[:conn]
+  
   
 end
